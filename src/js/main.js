4b295bccf0ff0f947727b0e1b0f6346fc398d727
Mahshid M <mahshid@Mahshids-MacBook-Pro.local>
2018-05-28 03:58:36 -0700

    slide slick

--- src/js/main.js
+++ src/js/main.js
@@ -17,3 +17,9 @@ $(document).on("click", '[data-toggle="lightbox"]', function(event) {
   event.preventDefault();
   $(this).ekkoLightbox();
 });
+
+$(".slider").slick({
+  infinite: true,
+  slideToShow: 1,
+  slideToScroll: 1
+});
